a46fc5070d7e39cc26dfe7f2b308d90aad736781
Justin Chase <justin.m.chase@gmail.com>
2023-08-06 14:05:17 -0500

    Add common event type

--- src/types/event.ts
+++ src/types/event.ts
@@ -1,6 +1,19 @@
 import { GitHubInstallationEvent } from "./installation.ts";
 import { GitHubPingEvent } from "./ping.ts";
+import { GitHubRepository } from "./repository.ts";
+import { GitHubOrg } from "./types.ts";
+import { GitHubUser } from "./user.ts";
+
+export type GitHubEventCommon = {
+  action?: string;
+  sender?: GitHubUser;
+  repository?: GitHubRepository;
+  organization?: GitHubOrg;
+};
 
 export type GitHubEvent =
-  | GitHubPingEvent
-  | GitHubInstallationEvent;
+  & GitHubEventCommon
+  & (
+    | GitHubPingEvent
+    | GitHubInstallationEvent
+  );
