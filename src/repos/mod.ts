8d9d72a06b1c682b51df20a54674744d25f3411d
Justin Chase <justin.m.chase@gmail.com>
2023-08-06 22:56:12 -0500

    0.5.0-pre.11

--- src/repos/mod.ts
+++ src/repos/mod.ts
@@ -1,6 +1,8 @@
 import { actions } from "./actions/mod.ts";
 import { commits } from "./commits/mod.ts";
+import { contents } from "./contents/mod.ts";
 export const repos = {
   actions,
   commits,
+  contents,
 };
