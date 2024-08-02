9ab82f0ea6317896c6d71d39a0cc7d911a37ba05
Justin Chase <justin.m.chase@gmail.com>
2024-08-02 11:05:50 -0500

    add issues to repos

--- src/repos/mod.ts
+++ src/repos/mod.ts
@@ -2,9 +2,11 @@ import { actions } from "./actions/mod.ts";
 import { commits } from "./commits/mod.ts";
 import { contents } from "./contents/mod.ts";
 import { pulls } from "./pulls/mod.ts";
+import { issues } from "./issues/mod.ts";
 export const repos = {
   actions,
   commits,
   contents,
+  issues,
   pulls,
 };
